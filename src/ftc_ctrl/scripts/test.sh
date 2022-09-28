a382d2d0855eaa45c6b00faa81f0ad19abe73b34
chankonixi <chengwy5@mail2.sysu.edu.cn>
2022-09-28 18:15:10 +0800

    expriment

--- src/ftc_ctrl/scripts/test.sh
+++ src/ftc_ctrl/scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# rostopic pub -1 $1/start_rotors std_msgs/Empty --
-
+rostopic pub -1 $1/start_rotors std_msgs/Empty --
+# awk -v va=10 'BEGIN{fe=1-exp(-va/1000);system("rostopic pub -r 250 hummingbird/reference_pos geometry_msgs/Point -- 0.0 -0.0 " fe);print fe;}'
 
 # ./reference.sh 0.0 -0.0 1.0 $1
 
@@ -9,11 +9,12 @@ function sigmoid()
 {
   local i=1
   while [ 0 -le $i ];do
-    rostopic pub -1 $1/start_rotors std_msgs/Empty --
-    awk -v va=$i -v vb=$1 'BEGIN{fe=1-exp(-va/1000);system("rostopic pub -1 " vb "/reference_pos geometry_msgs/Point -- 0.0 -0.0 " fe);print fe;}'
+    # rostopic pub -1 $1/start_rotors std_msgs/Empty --
+    awk -v va=$i 'BEGIN{fe=1-exp(-va/1000);system("rostopic pub -r 250 hummingbird/reference_pos geometry_msgs/Point -- 0.0 -0.0 " fe);print fe;}'
+    # awk -v va=10 'BEGIN{fe=1-exp(-va/1000);print fe;}'
     i=$[ $i + 1 ]
-    sleep 0.01
+    sleep 0.004
   done
 }
 
-sigmoid $1
+sigmoid
