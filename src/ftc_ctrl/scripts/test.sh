a5dd52f85c77285b7ba189ce25eeab1d2ce4bee2
chankonixi <chengwy5@mail2.sysu.edu.cn>
2022-10-15 20:36:22 +0800

    修改test.sh

--- src/ftc_ctrl/scripts/test.sh
+++ src/ftc_ctrl/scripts/test.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
 rostopic pub -1 $1/start_rotors std_msgs/Empty --
-
-
-# ./reference.sh 0.0 -0.0 1.0 $1
-
-# function sigmoid()
-# {
-#   local i=1
-#   while [ 0 -le $i ];do
-#     rostopic pub -1 $1/start_rotors std_msgs/Empty --
-#     awk -v va=$i -v vb=$1 'BEGIN{fe=1-exp(-va/1000);system("rostopic pub -1 " vb "/reference_pos geometry_msgs/Point -- 0.0 -0.0 " fe);print fe;}'
-#     i=$[ $i + 1 ]
-#     sleep 0.01
-#   done
-# }
-
-# sigmoid $1
